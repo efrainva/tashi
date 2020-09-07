84a929c0a9d46bc012c7a4c90cdfc9a4efb073c4
Efrain Vazquez <efrainvazquez@efrains-mbp.lan>
2020-09-06 21:06:06 -0400

    added aws files and config

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    aws_table_name: 'tashi_be',
+    // aws_local_config: {
+    //   region: 'local',
+    //   endpoint: 'http://localhost:8000'
+    // },
+    aws_remote_config: {
+        accessKeyId: 'AKIAZMA5U57UTZVDA3SN',
+        secretAccessKey: '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',
+        region: 'us-east-2',
+      }
+  };
+
+//   us-east-2
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const Course = require('./Course.json');
 const CourseOne = require('./CourseOne.json');
 const express = require('express')
 const cors = require("cors");
+const AWS = require('aws-sdk')
+const config = require('./config.js')
 
 const app = express()
 
@@ -11,13 +13,37 @@ app.use(cors())
 app.get('/',(req,res)=> {
     res.send(json)
 })
-app.get('/course/1',async(req,res)=> {
-    try{
-        res.send(Course)
-    }catch(err){
-        res.status(500).json({error:"error getting data"})
-    }
-})
+app.get('/one',async(req,res)=> {
+    console.log(config)
+    AWS.config.update(config.aws_remote_config);
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const params = {
+        TableName: config.aws_table_name
+    };
+
+    docClient.scan(params, function(err, data) {
+        if (err) {
+          res.send({
+            success: false,
+            message: 'Error: Server error'
+          });
+        } else {
+          const { Items } = data;
+          res.send({
+            success: true,
+            message: 'Loaded fruits',
+            fruits: Items
+          });
+        }
+      });
+    })
+
+    // try{
+    //     res.send(Course)
+    // }catch(err){
+    //     res.status(500).json({error:"error getting data"})
+    // }
+// })
 app.get('/course/2',async(req,res)=> {
     try{
         res.send(CourseOne)
@@ -26,4 +52,12 @@ app.get('/course/2',async(req,res)=> {
     }
 })
 
-module.exports=app
+
+
+
+
+
+
+
+
+module.exports = app
